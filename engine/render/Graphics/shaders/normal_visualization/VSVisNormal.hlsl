e7064d0daccd034e8083ca2c3dac66b26e20fb11
Ostap Trush <o.trush@dragonslake.com>
2023-03-03 15:08:12 +0100

    Fixed the normal vis shader to adapt for the normals pulled from model

--- engine/render/Graphics/shaders/normal_visualization/VSVisNormal.hlsl
+++ engine/render/Graphics/shaders/normal_visualization/VSVisNormal.hlsl
@@ -11,11 +11,15 @@ VS_OUTPUT main(VS_INPUT input)
 {
     VS_OUTPUT output;
     float4x4 modelToWorld = float4x4(input.modelToWorld0, input.modelToWorld1, input.modelToWorld2, input.modelToWorld3);
-    
+    float4x4 modelToWorldInv = float4x4(input.modelToWorldInv0, input.modelToWorldInv1, input.modelToWorldInv2, input.modelToWorldInv3);
+
+    float3 modelNorm = normalize(mul(float4(input.inNorm, 0.0f), transpose(meshToModelInv))).xyz;
     float4 modelPos = mul(float4(input.inPos, 1.0f), meshToModel);
     
     float4 worldPos = mul(modelPos, modelToWorld);
-    
+    float3 worldNorm = normalize(mul(float4(modelNorm, 0.0f), modelToWorldInv)).xyz;
+
     output.worldPos = worldPos.xyz;
+    output.worldNorm = worldNorm;
     return output;
 }
