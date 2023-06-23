b8ae9c75d46c6e289682c6fbb6a540081024797f
Ostap Trush <o.trush@dragonslake.com>
2023-06-23 13:02:29 +0200

    Wrote wrapper for compute shader

--- engine/render/Graphics/DXShaders/ComputeShader.cpp
+++ engine/render/Graphics/DXShaders/ComputeShader.cpp
@@ -4,13 +4,29 @@ namespace engn {
 	namespace rend {
 		void ComputeShader::init(const std::wstring& shaderPath)
 		{
+			if (!readShaderFile(shaderPath)) { return; }
+			if (!createShader(shaderPath)) { return; }
 		}
 		void ComputeShader::bind() const
 		{
+			d3d::s_devcon->CSSetShader(m_shader.Get(), NULL, 0);
 		}
 		bool ComputeShader::createShader(const std::wstring& shaderPath)
 		{
-			return false;
+			HRESULT hr = d3d::s_device->CreateComputeShader(
+				m_shaderBuffer->GetBufferPointer(),
+				m_shaderBuffer->GetBufferSize(),
+				NULL, // The pointer to class linkage
+				m_shader.GetAddressOf()
+			);
+
+			if (FAILED(hr)) {
+				Logger::instance().logErr("Failed to create Domain Shader " +
+					std::string(shaderPath.begin(), shaderPath.end()) + ": " + std::system_category().message(hr));
+				return false;
+			}
+
+			return true;
 		}
 	} // rend
 } // engn
--- engine/render/Graphics/DXShaders/ComputeShader.hpp
+++ engine/render/Graphics/DXShaders/ComputeShader.hpp
@@ -12,13 +12,12 @@ namespace engn {
 			void init(const std::wstring& shaderPath);
 			void bind() const;
 
-			[[nodiscard]] ID3D11DomainShader* getShader() const { return m_shader.Get(); };
+			[[nodiscard]] ID3D11ComputeShader* getShader() const { return m_shader.Get(); };
 		private:
 			//! Call create shader on d3d_device
 			bool createShader(const std::wstring& shaderPath);
 
-			Microsoft::WRL::ComPtr<ID3D11DomainShader> m_shader;
-		};
+			Microsoft::WRL::ComPtr<ID3D11ComputeShader> m_shader;
 		};
 	} // rend
 } // engn
